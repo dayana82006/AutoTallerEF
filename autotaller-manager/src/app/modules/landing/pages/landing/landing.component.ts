13150a69f703f28bb17045b9d5f63539fd66c575
merliariza <180667185+merliariza@users.noreply.github.com>
2025-06-18 17:36:34 -0500

    feat: :sparkles: add SCSS and TS with routing implementation for landing page
    Implemented the LandingComponent with its TypeScript logic, custom SCSS styling, and configured the route

--- autotaller-manager/src/app/modules/landing/pages/landing/landing.component.ts
+++ autotaller-manager/src/app/modules/landing/pages/landing/landing.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   standalone: false,
-  templateUrl: './landing.html',
-  styleUrl: './landing.scss'
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
 })
-export class Landing {
 
+export class LandingComponent {
+  generarMailto(): string {
+    const destinatario = 'automotriz@email.com';
+    const asunto = encodeURIComponent('Consulta desde la web');
+    const cuerpo = encodeURIComponent('Hola, me gustaría obtener más información sobre sus servicios.');
+
+    return `mailto:${destinatario}?subject=${asunto}&body=${cuerpo}`;
+  }
 }
