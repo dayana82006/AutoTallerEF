ede02a1f36db74ec93d118fabfd0db861671ed7f
merliariza <180667185+merliariza@users.noreply.github.com>
2025-06-18 18:07:47 -0500

    feat: :sparkles: add login component HTML, SCSS AND TS
    Created login component and routes implementation

--- autotaller-manager/src/app/app-routing-module.ts
+++ autotaller-manager/src/app/app-routing-module.ts
@@ -2,10 +2,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './modules/landing/pages/landing/landing.component';
-
+import { LoginComponent } from './modules/auth/pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'auth/login', component: LoginComponent },
 ];
 
 @NgModule({
