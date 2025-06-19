bdf3ec8b20eec484dc44fe092423ba52218fc1da
merliariza <180667185+merliariza@users.noreply.github.com>
2025-06-19 09:40:06 -0500

    feat: :sparkles: create admin navbar
    Created, structure, styles with bootstrap and routes logic for admin navbar

---
+++ autotaller-manager/src/app/modules/admin/components/admin-navbar/admin-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-navbar',
+  standalone: false,
+  templateUrl: './admin-navbar.html',
+  styleUrl: './admin-navbar.scss'
+})
+export class AdminNavbar {
+
+}
