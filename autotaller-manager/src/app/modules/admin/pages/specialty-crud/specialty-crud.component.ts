6d07b96cb4b378d8f008dbaa68487d54a42d4e32
merliariza <180667185+merliariza@users.noreply.github.com>
2025-06-20 18:22:47 -0500

    feat: :sparkles: add specialities crud validations
    Created validations to improve user experience

--- autotaller-manager/src/app/modules/admin/pages/specialty-crud/specialty-crud.component.ts
+++ autotaller-manager/src/app/modules/admin/pages/specialty-crud/specialty-crud.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Specialty } from '../../../../models/specialty.model';
 import { MockSpecialtyService } from '../../../../core/services/mock-specialty';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-specialty-crud',
   templateUrl: './specialty-crud.component.html',
@@ -10,50 +11,76 @@ import { CommonModule } from '@angular/common';
 })
 export class SpecialtyCrudComponent implements OnInit {
   specialties: Specialty[] = [];
+  allSpecialties: Specialty[] = [];
   newSpecialty: Specialty = { id: 0, name: '' };
   editMode = false;
+  search: string = '';
+
+  @ViewChild('formSection') formSection!: ElementRef;
 
   constructor(private specialtyService: MockSpecialtyService) {}
-search: string = '';
-allSpecialties: Specialty[] = [];
-
-
-
-loadSpecialties() {
-  this.specialtyService.getAll().subscribe(data => {
-    this.specialties = data;
-    this.allSpecialties = [...data]; 
-  });
-}
-
-onSearchChange() {
-  const term = this.search.toLowerCase();
-  this.specialties = this.allSpecialties.filter(s =>
-    s.name.toLowerCase().includes(term)
-  );
-}
 
   ngOnInit(): void {
     this.loadSpecialties();
   }
-  
+
+  loadSpecialties() {
+    this.specialtyService.getAll().subscribe(data => {
+      this.specialties = data;
+      this.allSpecialties = [...data];
+    });
+  }
+
+  onSearchChange() {
+    const term = this.search.toLowerCase();
+    this.specialties = this.allSpecialties.filter(s =>
+      s.name.toLowerCase().includes(term)
+    );
+  }
+
   save() {
-    if (this.editMode) {
-      this.specialtyService.update(this.newSpecialty.id, this.newSpecialty).subscribe(() => {
-        this.loadSpecialties();
-        this.cancel();
-      });
-    } else {
-      this.specialtyService.create(this.newSpecialty).subscribe(() => {
-        this.loadSpecialties();
-        this.cancel();
-      });
-    }
+  const trimmedName = this.newSpecialty.name?.trim().toLowerCase();
+
+  if (!trimmedName || trimmedName.length < 3) {
+    alert('El nombre de la especialidad es obligatorio y debe tener al menos 3 caracteres.');
+    return;
+  }
+
+  const nameExists = this.allSpecialties.some(s =>
+    s.name.toLowerCase() === trimmedName && s.id !== this.newSpecialty.id
+  );
+
+  if (nameExists) {
+    alert('Ya existe una especialidad con ese nombre.');
+    return;
+  }
+
+  if (this.editMode && this.newSpecialty.id > 0) {
+    this.specialtyService
+      .update(this.newSpecialty.id, { ...this.newSpecialty, name: trimmedName })
+      .subscribe(() => this.resetForm());
+  } else {
+    this.specialtyService
+      .create({ ...this.newSpecialty, name: trimmedName })
+      .subscribe(() => this.resetForm());
+  }
+}
+
+
+  resetForm() {
+    this.newSpecialty = { id: 0, name: '' };
+    this.editMode = false;
+    this.loadSpecialties();
   }
 
   edit(s: Specialty) {
     this.newSpecialty = { ...s };
     this.editMode = true;
+
+    // Hacer scroll al formulario
+    setTimeout(() => {
+      this.formSection?.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, 0);
   }
 
   delete(id: number) {
@@ -63,8 +90,6 @@ onSearchChange() {
   }
 
   cancel() {
-    this.newSpecialty = { id: 0, name: '' };
-    this.editMode = false;
+    this.resetForm();
   }
-  
 }
