7675a3ff85dc20ccbb3bcaa061d769a7ac9297e6
Juan Caballero <juandacf@gmail.com>
2025-06-22 14:39:37 -0500

    fix: search bar

---
+++ autotaller-manager/src/app/modules/admin/models/order-service.ts
@@ -0,0 +1,9 @@
+import {Vehicle} from './vehicle';
+import { ServiceType } from './service-type';
+import { User } from './user';
+
+export interface OrderService {
+    id: number,
+    description: string,
+    
+}
